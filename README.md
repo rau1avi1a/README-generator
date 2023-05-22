ac2ba410e598fd446c78dd0974e856f5968cd7b2
rau1avi1a <avila0118@icloud.com>
2023-05-21 21:06:42 -0700

    added badges to the js function and updated README file

--- README.md
+++ README.md
@@ -1,17 +1,22 @@
-# title test
+# README-generator 
 ## Description
-desc test
+This application creates a professional README file for the user using inquirer.
 ## Table of Contents
-table test
+1. [Installation](#https://github.com/rau1avi1a/README-generator#installation)
+2. [Usage](#https://github.com/rau1avi1a/README-generator#usage)
+3. [License](#https://github.com/rau1avi1a/README-generator#license)
+4. [Contributing](#https://github.com/rau1avi1a/README-generator#contributing)
+5. [Tests](https://github.com/rau1avi1a/README-generator#tests)
+6. [Questions?](https://github.com/rau1avi1a/README-generator#questions)
 ## Installation
-installation guide
+This application uses Node.js in order to create README file. inquirer can be installed using '$ npm i'
 ## Usage
-usage...
-## Lisence
-The Unlicense
+In order to use this application, install inquirer and all its requirements using '\$ npm i' on the console and the run the js file on the console using '$ node index.js'. Fill out the README file sections to create the README file! If a section is not applicable, N/A can be used.
+## License
+N/A
 ## Contributing
-idk
+N/A
 ## Tests
-test
+N/A
 ## Questions?
 Reach me on [GitHub](https://github.com/rau1avi1a)
--- index.js
+++ index.js
@@ -18,11 +18,6 @@ const questions = [
     },
     {
         type: 'input',
-        name: 'table',
-        message: 'Table of Contents'
-    },
-    {
-        type: 'input',
         name: 'installation',
         message: 'Installation'
     },
@@ -35,7 +30,7 @@ const questions = [
         type: 'list',
         name: 'license',
         message: 'Choose a License',
-        choices: ['Apache License 2.0', 'GNU General Public License v3.0', 'MIT License', 'BSD 2-Clause "Simplified" License', 'BSD 3-Clause "New" or "Revised" License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal', 'Eclipse Public License 2.0', 'GNU Affero General Public License v3.0', 'GNU General Public License v2.0', 'GNU Lesser General Public v2.1', 'Mozilla Public License 2.0', 'The Unlicense']
+        choices: ['Apache License 2.0', 'GNU General Public License v3.0', 'MIT License', 'BSD 2-Clause "Simplified" License', 'BSD 3-Clause "New" or "Revised" License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal', 'Eclipse Public License 2.0', 'GNU Affero General Public License v3.0', 'GNU General Public License v2.0', 'GNU Lesser General Public v2.1', 'Mozilla Public License 2.0', 'The Unlicense', 'None']
     },
     {
         type: 'input',
@@ -57,6 +52,57 @@ const questions = [
 // TODO: Create a function to initialize app
 function init() {
     inquirer.prompt(questions).then(function(answers) {
+        if (answers.license == 'Apache License 2.0') {
+            answers.license = `Apache License 2.0 [![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+            licenseBadge = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+        } else if (answers.license == 'GNU General Public License v3.0') {
+            answers.license = `GNU General Public License v3.0 [![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
+            licenseBadge = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+        } else if (answers.license == 'MIT License') {
+            answers.license = `MIT License [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+            licenseBadge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+        }
+        else if(answers.license == 'BSD 2-Clause "Simplified" License') {
+            answers.license = `BSD 2-Clause "Simplified" License [![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)`;
+            licenseBadge = '[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)';
+        }
+        else if(answers.license == 'BSD 3-Clause "New" or "Revised" License') {
+            answers.license = `BSD 3-Clause "New" or "Revised" License [![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`;
+            licenseBadge = '[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)';
+        }
+        else if(answers.license == 'Boost Software License 1.0') {
+            answers.license = `Boost Software License 1.0 [![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)`;
+            licenseBadge = '[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)';
+        }
+        else if(answers.license == 'Creative Commons Zero v1.0 Universal') {
+            answers.license = `Creative Commons Zero v1.0 Universal [![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/)`;
+            licenseBadge = '[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/)';
+        }
+        else if(answers.license == 'Eclipse Public License 2.0') {
+            answers.license = `Eclipse Public License 2.0 [![License](https://img.shields.io/badge/License-EPL_2.0-red.svg)](https://www.eclipse.org/legal/epl-2.0/)`;
+            licenseBadge = '[![License](https://img.shields.io/badge/License-EPL_2.0-red.svg)](https://www.eclipse.org/legal/epl-2.0/)';
+        }
+        else if(answers.license == 'GNU Affero General Public License v3.0') {
+            answers.license = `GNU Affero General Public License v3.0 [![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)`;
+            licenseBadge = '[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)';
+        }
+        else if(answers.license == 'GNU General Public License v2.0') {
+            answers.license = `GNU General Public License v2.0 [![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)`;
+            licenseBadge = '[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)';
+        }
+        else if(answers.license == 'Mozilla Public License 2.0') {
+            answers.license = `Mozilla Public License 2.0 [![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`;
+            licenseBadge = '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)';
+        }
+        else if(answers.license == 'The Unlicense') {
+            answers.license = `The Unlicense [![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)`;
+            licenseBadge = '[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)';
+        }
+        else if(answers.license == 'None') {
+            answers.license = 'N/A';
+            licenseBadge = '';
+        }
+
         const markdownText = generateMarkdown(answers);
         fs.writeFile('README.md', markdownText, (err) =>
       err ? console.log(err) : console.log('Successfully created README file!')
