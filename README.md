11d61b6ddebd79f054eccf3488efbb10180ecfcd
rau1avi1a <avila0118@icloud.com>
2023-05-21 21:33:12 -0700

    added tutorial to README file

--- README.md
+++ README.md
@@ -2,15 +2,17 @@
 ## Description
 This application creates a professional README file for the user using inquirer.
 ## Table of Contents
-1. [Installation](#https://github.com/rau1avi1a/README-generator#installation)
-2. [Usage](#https://github.com/rau1avi1a/README-generator#usage)
-3. [License](#https://github.com/rau1avi1a/README-generator#license)
-4. [Contributing](#https://github.com/rau1avi1a/README-generator#contributing)
+1. [Installation](https://github.com/rau1avi1a/README-generator#installation)
+2. [Usage](https://github.com/rau1avi1a/README-generator#usage)
+3. [License](https://github.com/rau1avi1a/README-generator#license)
+4. [Contributing](https://github.com/rau1avi1a/README-generator#contributing)
 5. [Tests](https://github.com/rau1avi1a/README-generator#tests)
 6. [Questions?](https://github.com/rau1avi1a/README-generator#questions)
 ## Installation
 This application uses Node.js in order to create README file. inquirer can be installed using '$ npm i'
 ## Usage
+<img src="./assets/README-generator-tutorial.gif"/>
+
 In order to use this application, install inquirer and all its requirements using '\$ npm i' on the console and the run the js file on the console using '$ node index.js'. Fill out the README file sections to create the README file! If a section is not applicable, N/A can be used.
 ## License
 N/A
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,27 +1,13 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {
-};
-
 // TODO: Create a function to generate markdown for README
-
 function generateMarkdown(data) {
   return `# ${data.title} ${licenseBadge}
 ## Description
 ${data.description}
 ## Table of Contents
-1. [Installation](#https://github.com/${data.questions}/${data.title}#installation)
-2. [Usage](#https://github.com/${data.questions}/${data.title}#usage)
-3. [License](#https://github.com/${data.questions}/${data.title}#license)
-4. [Contributing](#https://github.com/${data.questions}/${data.title}#contributing)
+1. [Installation](https://github.com/${data.questions}/${data.title}#installation)
+2. [Usage](https://github.com/${data.questions}/${data.title}#usage)
+3. [License](https://github.com/${data.questions}/${data.title}#license)
+4. [Contributing](https://github.com/${data.questions}/${data.title}#contributing)
 5. [Tests](https://github.com/${data.questions}/${data.title}#tests)
 6. [Questions?](https://github.com/${data.questions}/${data.title}#questions)
 ## Installation
